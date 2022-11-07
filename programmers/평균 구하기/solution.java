8501753f2170b85a044202ea9825a370de484743
JEONG SEOK CHOI <109507834+JSCHOISNOTE@users.noreply.github.com>
2022-11-07 13:37:02 +0900

    uploaded at 2022. 11. 7. 오후 1:39:33

---
+++ programmers/평균 구하기/solution.java
@@ -0,0 +1,12 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12944
+
+class Solution {
+    public double solution(int[] arr) {
+        double sum = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        return sum / arr.length;
+    }
+}
