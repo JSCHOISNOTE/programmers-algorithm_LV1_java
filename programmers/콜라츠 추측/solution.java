70f0c7c6edf82383d485106935de5f1bb5e14c21
JEONG SEOK CHOI <109507834+JSCHOISNOTE@users.noreply.github.com>
2022-11-29 13:21:19 +0900

    uploaded at 2022. 11. 29. 오후 1:24:34

---
+++ programmers/콜라츠 추측/solution.java
@@ -0,0 +1,18 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12943
+
+class Solution {
+    public int solution(int num) {
+        long number = num;
+        int answer;
+        
+        for(answer = 0; answer < 500; answer++){
+            if(number == 1) return answer;
+            else{
+                if(number % 2 == 0) number /= 2;
+                else number = number * 3 + 1;
+            }
+        } 
+        
+        return -1;
+    }
+}
