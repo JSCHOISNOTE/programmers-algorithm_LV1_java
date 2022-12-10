7fe53333f7f31bf0696ab8d233f6390111c01dc7
JEONG SEOK CHOI <109507834+JSCHOISNOTE@users.noreply.github.com>
2022-12-10 16:11:49 +0900

    uploaded at 2022. 12. 10. 오후 4:11:53

---
+++ programmers/가운데 글자 가져오기/solution.java
@@ -0,0 +1,15 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12903
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        if(s.length() % 2 == 0){ //짝수
+            answer = s.substring(s.length()/2-1, s.length()/2+1);
+        }else { //홀수
+            answer = s.substring(s.length()/2, s.length()/2+1);
+        }
+
+        return answer;
+    }
+}
