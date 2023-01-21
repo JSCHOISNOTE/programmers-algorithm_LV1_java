a05340ff3a141e973fe6362323e2b71f680655c9
JEONG SEOK CHOI <109507834+JSCHOISNOTE@users.noreply.github.com>
2023-01-21 16:48:35 +0900

    uploaded at 2023. 1. 21. 오후 4:49:51

---
+++ programmers/문자열 내 마음대로 정렬하기/solution.java
@@ -0,0 +1,22 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12915
+
+import java.util.*;
+
+class Solution {
+  public String[] solution(String[] strings, int n) {
+      String[] answer = {};
+        answer = new String[strings.length];
+        int cnt = 0;
+        
+        Arrays.sort(strings);
+        
+        for(int i = 97; i < 123; i++) {
+            for(int j = 0; j < strings.length; j++) {
+                if(strings[j].charAt(n) == (char)i) {
+                    answer[cnt++] = strings[j];
+                }
+            }
+        }
+        return answer;
+  }
+}
