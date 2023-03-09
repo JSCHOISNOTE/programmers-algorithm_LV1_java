0281200b3cacca7640106cf42b47ce3fbe08eaf0
JEONG SEOK CHOI <109507834+JSCHOISNOTE@users.noreply.github.com>
2023-03-09 14:12:23 +0900

    uploaded at 2023. 3. 9. 오후 2:13:52

---
+++ programmers/햄버거 만들기/solution.java
@@ -0,0 +1,31 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/133502
+
+public class Solution {
+	
+	// 햄버거 만들기
+	// 빵(1), 야채(2), 고기(3), 빵(1)일 경우 햄버거 완성
+	
+	public static int solution(int[] ingredient) {
+		int answer = 0;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < ingredient.length; i++) {
+			sb.append(ingredient[i]);
+			
+			if (sb.length() > 3 && sb.subSequence(sb.length() - 4, sb.length()).equals("1231")) {
+				sb.delete(sb.length() - 4, sb.length());
+				answer++;
+			}
+		}
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] ingredient = {2, 1, 1, 2, 3, 1, 2, 3, 1};
+		
+		System.out.println(solution(ingredient));
+	}
+}
